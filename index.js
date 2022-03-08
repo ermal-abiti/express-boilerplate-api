b9f96379a879a000970a85df093c7cf7c7a426bc
ermalabiti <ermal.abiti@gmail.com>
2022-03-08 16:08:09 +0100

    update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('*', (req, res) => {
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 
 server.listen(PORT, () => {
